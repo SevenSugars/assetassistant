fb05094065e96c4727dc2aff2980c0308340e624
Cheng <327798614@qq.com>
2018-05-21 07:35:54 +0800

    news

--- home/models.py
+++ home/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=30)  #用户名
     password = models.CharField(max_length=30)  #密码
+    emailaddress = models.EmailField(error_messages={'invalid': '请输入正确的邮箱格式'})  #邮箱
 
 #新闻
 class News(models.Model):
@@ -15,70 +16,70 @@ class News(models.Model):
 
 #推荐
 class Recommend(models.Model):
-    code = models.IntegerField      #推荐基金、股票、代码
+    code = models.IntegerField()      #推荐基金、股票、代码
     name = models.CharField(max_length=30)      #名字
-    annualrate = models.FloatField      #年涨跌幅
+    annualrate = models.FloatField()      #年涨跌幅
 
 #股票
-class stock(models.Model):
-    code = models.IntegerField      #代码
+class Stock(models.Model):
+    code = models.IntegerField()      #代码
     name = models.CharField(max_length=30)      #名字
-    price = models.FloatField      #当前价格
-    open = models.FloatField       #今日开盘价
-    close = models.FloatField      #今日收盘价
-    high = models.FloatField       #今日最高价
-    low = models.FloatField        #今日最低价
-    currentrate = models.FloatField     #当前涨跌幅
+    price = models.FloatField()      #当前价格
+    open = models.FloatField()       #今日开盘价
+    close = models.FloatField()      #今日收盘价
+    high = models.FloatField()       #今日最高价
+    low = models.FloatField()        #今日最低价
+    currentrate = models.FloatField()     #当前涨跌幅
 
 #基金
-class fund(models.Model):
-    code = models.IntegerField      #代码
+class Fund(models.Model):
+    code = models.IntegerField()      #代码
     name = models.CharField(max_length=30)      #名字
-    price = models.FloatField      #当前价格
-    currentrate = models.FloatField     #当前涨跌幅
-    onemrate = models.FloatField      #近1月收益率
-    threemrate = models.FloatField    #近3月收益率
-    sixmrate = models.FloatField      #近6月收益率
-    annualrate = models.FloatField  #年收益率
+    price = models.FloatField()      #当前价格
+    currentrate = models.FloatField()     #当前涨跌幅
+    onemrate = models.FloatField()      #近1月收益率
+    threemrate = models.FloatField()    #近3月收益率
+    sixmrate = models.FloatField()      #近6月收益率
+    annualrate = models.FloatField()  #年收益率
 
 #收藏
-class favourite(models.Model):
+class Favourite(models.Model):
     username = models.CharField(max_length=30)  #用户名
-    code = models.IntegerField      #代码
+    code = models.IntegerField()      #代码
     name = models.CharField(max_length=30)      #名字
 
 #当前持有
-class own(models.Model):
+class Own(models.Model):
     username = models.CharField(max_length=30)  #用户名
-    code = models.IntegerField      #代码
+    code = models.IntegerField()      #代码
     name = models.CharField(max_length=30)      #名字
-    volumn = models.FloatField      #持有量
-    buy = models.FloatField         #买入价
+    volume = models.FloatField()      #持有量
+    buy = models.FloatField()         #买入价
 
 #历史交易
-class hist_trade(models.Model):
+class Hist_trade(models.Model):
     username = models.CharField(max_length=30)  #用户名
-    code = models.IntegerField      #代码
+    code = models.IntegerField()      #代码
     name = models.CharField(max_length=30)      #名字
-    volumn = models.FloatField      #交易量
-    price = models.FloatField       #交易价格
-    time = models.TimeField         #交易时间
+    volume = models.FloatField()      #交易量
+    price = models.FloatField()       #交易价格
+    time = models.DateTimeField()         #交易时间
 
 #按操作结算资产
-class prosenal_asset(models.Model):
+class Prosenal_asset(models.Model):
     username = models.CharField(max_length=30)  #用户名
-    stock = models.FloatField       #股票资产
-    stockprofit = models.FloatField #股票收益
-    fund = models.FloatField        #基金资产
-    fundprofit = models.FloatField  #基金收益
-    money = models.FloatField       #现金资产
-    time = models.TimeField         #交易时间
+    stock = models.FloatField()       #股票资产
+    stockprofit = models.FloatField() #股票收益
+    fund = models.FloatField()        #基金资产
+    fundprofit = models.FloatField()  #基金收益
+    money = models.FloatField()       #现金资产
+    time = models.TimeField()         #交易时间
 
 #按日结算资产
-class hist_asset(models.Model):
+class Hist_asset(models.Model):
     username = models.CharField(max_length=30)  #用户名
-    stock = models.FloatField       #股票资产
-    fund = models.FloatField        #基金资产
-    money = models.FloatField       #现金资产
-    time = models.TimeField         #交易时间
+    stock = models.FloatField()       #股票资产
+    fund = models.FloatField()        #基金资产
+    money = models.FloatField()       #现金资产
+    time = models.TimeField()         #交易时间
 
--- home/views.py
+++ home/views.py
@@ -1,23 +1,32 @@
 from django.shortcuts import render
+from . import models
+import tushare as ts
+import pandas as pd
 
-def regist():
+pd.set_option('max_colwidth', 20000)
+
+def regist(request):
     pass
 
-def login():
+def login(request):
     pass
 
-def news():
+def news(request):
+    news = models.News()
+    news.title = ts.get_latest_news(top=1, show_content=True).title.to_string(index=False)
+    news.content = ts.get_latest_news(top=1, show_content=True).content.to_string(index=False)
+    news.save()
+    return render(request, 'news.html', {'news':news})
+
+def recommend(request):
     pass
 
-def recommend():
+def tutorial(request):
     pass
 
-def tutorial():
+def showstock(request):
     pass
 
-def showstock():
-    pass
-
-def showfund():
+def showfund(request):
     pass
 
